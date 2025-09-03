3b610c3062792172db95c10be7e63308f9c76f74
darkh <yazidsaid5@gmail.com>
2025-09-03 13:11:13 -0500

    feat(greet-developer): add incoming and outgoing resources

---
+++ Acme.Hello.Platform/Generic/Domain/Intefaces/REST/Resources/GreetDeveloperRequest.cs
@@ -0,0 +1,10 @@
+﻿namespace Acme.Hello.Platform.Generic.Domain.Intefaces.REST.Resources;
+/// <summary>
+/// Request to greet a developer.
+/// </summary>
+/// <remarks>
+///This record is used in the GreetDeveloper endpoint to capture the first and last name of the developer.
+/// </remarks>
+/// <param name="firstName"> The first name of the developer to be greeted. Optional</param>
+/// <param name="lastName"> The last name of the developer to be greeted. Optional</param>
+public record GreetDeveloperRequest(string? firstName, string? lastName);
---
+++ Acme.Hello.Platform/Generic/Domain/Intefaces/REST/Resources/GreetDeveloperResponse.cs
@@ -0,0 +1,26 @@
+﻿using Acme.Hello.Platform.Generic.Domain.Model.Entities;
+
+namespace Acme.Hello.Platform.Generic.Domain.Intefaces.REST.Resources;
+/// <summary>
+/// Response returned when greeting a developer.
+/// </summary>
+/// <remarks>
+/// This record encapsulates the response details 
+/// </remarks>
+/// <param name="Id"></param>
+/// <param name="FullName"></param>
+/// <param name="Message"></param>
+public record GreetDeveloperResponse(Guid? Id, string? FullName, string? Message)
+{
+    /// <summary>
+    /// Initializes a new instance of the <see cref="GreetDeveloperResponse"/> class.
+    /// </summary>
+    /// <remarks>
+    ///This constructor is used to create a response with a message.
+    /// </remarks>
+    /// <param name="message">The mess</param>
+    public GreetDeveloperResponse(string message) : this(null, null, message)
+    {
+        
+    }
+}
