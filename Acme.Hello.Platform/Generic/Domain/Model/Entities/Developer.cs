dda50b56a7029a0e5caad418e40ff1eb62764c91
darkh <yazidsaid5@gmail.com>
2025-09-03 12:55:07 -0500

    chore:  default setup

---
+++ Acme.Hello.Platform/Generic/Domain/Model/Entities/Developer.cs
@@ -0,0 +1,14 @@
+﻿namespace Acme.Hello.Platform.Generic.Domain.Model.Entities;
+
+public class Developer(string? firstName, string? lastName)
+{
+    public Guid Id {get; } = Guid. NewGuid(); 
+    public string FirstName { get; } = string.IsNullOrWhiteSpace(firstName) ?string.Empty : firstName.Trim();
+    public string LastName { get; } = string.IsNullOrWhiteSpace(lastName) ? string.Empty : lastName.Trim();
+    /// <summary>
+    /// Gets the full name of the developer by concatenating the first and last name.
+    /// </summary>
+    /// <returns></returns>
+    public string GetFullName() => $"{FirstName} {LastName}".Trim();
+    public bool IsAnyNameEmpty() => string.IsNullOrEmpty(FirstName) || string.IsNullOrEmpty(LastName);
+}
